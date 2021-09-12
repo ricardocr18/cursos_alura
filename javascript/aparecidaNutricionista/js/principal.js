83e893b4e37ddf6c0f0bf681c16c99f06590d034
Ricardo Correa Ribeiro <ricardocribeiro@hotmail.com>
2021-09-12 20:13:26 -0300

    calculando o imc de um paciente

--- javascript/aparecidaNutricionista/js/principal.js
+++ javascript/aparecidaNutricionista/js/principal.js
@@ -1,3 +1,13 @@
-
-var titulo = document.querySelector(".titulo");
+let titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
+
+let paciente = document.querySelector("#primeiro-paciente");
+
+let tdPeso = paciente.querySelector(".info-peso");
+let peso = tdPeso.textContent;
+
+let tdAltura = paciente.querySelector(".info-altura");
+let altura = tdAltura.textContent;
+
+let imc = peso /(altura * altura);
+console.log(imc);
