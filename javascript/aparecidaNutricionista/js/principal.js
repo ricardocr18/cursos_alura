e9a811e0ac480936f645ceb4cd8486db941ea4ed
Ricardo Correa Ribeiro <ricardocribeiro@hotmail.com>
2021-09-12 23:15:07 -0300

    usando o FOR para percorre todo o Array e usando o CSS

--- javascript/aparecidaNutricionista/js/principal.js
+++ javascript/aparecidaNutricionista/js/principal.js
@@ -1,13 +1,44 @@
 let titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-let paciente = document.querySelector("#primeiro-paciente");
+let pacientes = document.querySelectorAll(".paciente");  // aqui vejo todos as pacientes no meu Array
+for (let i = 0; i < pacientes.length; i++) {
 
-let tdPeso = paciente.querySelector(".info-peso");
-let peso = tdPeso.textContent;
+    var paciente = pacientes[i];
 
-let tdAltura = paciente.querySelector(".info-altura");
-let altura = tdAltura.textContent;
+    let tdPeso = paciente.querySelector(".info-peso");
+    let peso = tdPeso.textContent;
 
-let imc = peso /(altura * altura);
-console.log(imc);
+    let tdAltura = paciente.querySelector(".info-altura");
+    let altura = tdAltura.textContent;
+
+    let tdImc = paciente.querySelector(".info-imc");
+    // Validando Peso e Altura
+
+    let pesoEhValido = true;
+    let alturaEhValida = true;
+
+    if (peso <= 0 || peso >= 300) {
+        pesoEhValido = false;
+        tdImc.textContent = "Peso inválido";
+        paciente.classList.add("paciente-invalido"); // Aqui mudo a cor conforme  CSS está configurado
+
+    }
+
+    if (altura <= 0 || altura >= 3.00) {
+        alturaEhValida = false;
+        tdImc.textContent = "Altura inválido";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if (alturaEhValida && pesoEhValido) {
+        let imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2); //o toFixed() descreve quantas casas decimais serão aplicadas
+    }
+
+}
+
+
+
+// Fim do Validador Peso e Altura
+
