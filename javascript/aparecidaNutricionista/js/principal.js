81d84099fcd487a7d2b3467c3a23a66d78c841c0
Ricardo Correa Ribeiro <ricardocribeiro@hotmail.com>
2021-09-13 02:12:44 -0300

    cadastrar usuário pelo Form na tabela pacientes

--- javascript/aparecidaNutricionista/js/principal.js
+++ javascript/aparecidaNutricionista/js/principal.js
@@ -35,8 +35,42 @@ for (let i = 0; i < pacientes.length; i++) {
         let imc = peso / (altura * altura);
         tdImc.textContent = imc.toFixed(2); //o toFixed() descreve quantas casas decimais serão aplicadas
     }
-
 }
+
+// Vamos adicionar um paciente na tabela
+let botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function(event){
+    event.preventDefault();
+    
+    let form = document.querySelector('#form-adiciona');  // PEgando os dados digitados no forme
+    var nome =form.nome.value;
+    var peso =form.peso.value;
+    var altura =form.altura.value;
+    var gordura =form.gordura.value;
+
+    var pacienteTr = document.createElement('tr');
+
+    var nomeTd  = document.createElement('td');
+    var pesoTd  = document.createElement('td');
+    var alturaTd  = document.createElement('td');
+    var gorduraTd  = document.createElement('td');
+    var imcTd  = document.createElement('td');
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+
+    var tabela = document.querySelector('#tabela-pacientes');
+    tabela.appendChild(pacienteTr);
+    
+});
+
 
 
 
