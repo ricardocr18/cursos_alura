0dc0668fcebdb26cb72ac18109cc862752596daa
Ricardo Correa Ribeiro <ricardocribeiro@hotmail.com>
2021-09-24 01:44:25 -0300

    criação de métodos e palavra chave this

--- javascript/ByteBank/index.js
+++ javascript/ByteBank/index.js
@@ -1,21 +1,39 @@
 class Cliente {
     nome;
     cpf;
+}
+
+class ContaCorrente{
     agencia;
-    saldo;
+    // #saldo - https://github.com/tc39/proposal-class-fields#private-fields
+    _saldo = 0;
+
+    sacar(valor){
+        if(this._saldo >= valor ) {
+            this._saldo -= valor
+            return valor;
+        }
+    }
+
+    depositar(valor){
+        if(valor <= 0) return;
+        this._saldo += valor;
+    }    
 }
 
 const cliente1 = new Cliente();
+    cliente1.nome = 'Ricardo';
+    cliente1.cpf = 1717171717;
+
 const cliente2 = new Cliente();
+    cliente2.nome = 'Alice';
+    cliente2.cpf = 696969696969;
 
-cliente1.nome = 'Ricardo';
-cliente1.cpf = 1717171717;
-cliente1.agencia = 171;
-cliente1.saldo = 0;
+const contaCorrenteRicardo = new ContaCorrente()
+    contaCorrenteRicardo.agencia = 1001;
 
-cliente2.nome = 'Alice';
-cliente2.cpf = 696969696969;
-cliente2.agencia = 171;
-cliente2.saldo = 0;
+contaCorrenteRicardo.depositar(300);
+const valorSacado =  contaCorrenteRicardo.sacar(10);
+console.log(valorSacado);
 
-console.log(cliente1, cliente2);
+console.log(contaCorrenteRicardo);
