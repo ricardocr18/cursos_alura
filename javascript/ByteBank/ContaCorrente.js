56e18401d13a2de29fdb73475014654d188d7d90
Ricardo Correa Ribeiro <ricardocribeiro@hotmail.com>
2021-09-25 22:45:11 -0300

    Alterando dinamicamente um objeto

---
+++ javascript/ByteBank/ContaCorrente.js
@@ -0,0 +1,25 @@
+export class ContaCorrente{
+    agencia;
+    cliente;
+
+    // #saldo - https://github.com/tc39/proposal-class-fields#private-fields
+    _saldo = 0;
+
+    sacar(valor){
+        if(this._saldo >= valor ) {
+            this._saldo -= valor
+            return valor;
+        }
+    }
+
+    depositar(valor){
+        if(valor <= 0) return;
+        this._saldo += valor;
+    }
+    
+    tranasferir(valor, conta){
+        const valorSacado = this.sacar(valor);
+        conta.depositar(valorSacado);
+    }
+
+}
--- javascript/ByteBank/index.js
+++ javascript/ByteBank/index.js
@@ -1,25 +1,5 @@
-class Cliente {
-    nome;
-    cpf;
-}
-
-class ContaCorrente{
-    agencia;
-    // #saldo - https://github.com/tc39/proposal-class-fields#private-fields
-    _saldo = 0;
-
-    sacar(valor){
-        if(this._saldo >= valor ) {
-            this._saldo -= valor
-            return valor;
-        }
-    }
-
-    depositar(valor){
-        if(valor <= 0) return;
-        this._saldo += valor;
-    }    
-}
+import {Cliente} from './Cliente.js'
+import { ContaCorrente } from './ContaCorrente.js';
 
 const cliente1 = new Cliente();
     cliente1.nome = 'Ricardo';
@@ -29,11 +9,16 @@ const cliente2 = new Cliente();
     cliente2.nome = 'Alice';
     cliente2.cpf = 696969696969;
 
-const contaCorrenteRicardo = new ContaCorrente()
+const contaCorrenteRicardo = new ContaCorrente();
     contaCorrenteRicardo.agencia = 1001;
+    contaCorrenteRicardo.cliente = cliente1;
+    contaCorrenteRicardo.depositar(500);
 
-contaCorrenteRicardo.depositar(300);
-const valorSacado =  contaCorrenteRicardo.sacar(10);
-console.log(valorSacado);
+const conta2 = new ContaCorrente();
+conta2.cliente = cliente2;
+conta2.agencia = 102;
 
+contaCorrenteRicardo.tranasferir(200, conta2);
+
+console.log(conta2);
 console.log(contaCorrenteRicardo);
