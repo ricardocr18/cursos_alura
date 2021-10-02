8d35dfd1ac14b4334f0d2ff35193c4bcea3e4b1e
Ricardo Correa Ribeiro <ricardocribeiro@hotmail.com>
2021-10-02 20:34:56 -0300

    configuração do CSS e tratamento de conflito de nomes

--- react/ceep/src/App.js
+++ react/ceep/src/App.js
@@ -1,17 +1,22 @@
-import React, {Component} from 'react';
-import ListaDeNotas from "./components/ListasDeNotas";
-import FormularioCadastro from "./components/FormularioCadastro";
-
+import React, { Component } from "react";
+import ListaDeNotas from "./components/ListaDeNotas";
+import FormularioCadastro from './components/FormularioCadastro';
+import "./assets/App.css";
+import './assets/index.css';
 class App extends Component {
 
-  render(){
+  criarNota(titulo, texto){
+    console.log(`uma nova nota foi criada ` + titulo + " " + texto)
+  }
+  render() {
     return (
-      <section>
-        <FormularioCadastro/>
-        <ListaDeNotas/>
-      </section>  
+      <section className="conteudo">
+        <FormularioCadastro criarNota={this.criarNota}/>
+        <ListaDeNotas />
+      </section>
     );
-  }  
+  }
 }
+
 
 export default App;
