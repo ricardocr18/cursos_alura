0156357a558f77dae0c1f51cba962f9412580051
Ricardo Correa Ribeiro <ricardocribeiro@hotmail.com>
2021-10-23 20:05:29 -0300

    Formulário mais completo

---
+++ react/formulario_completo/src/components/FromularioCadastro/FormularioCadastro.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import DadosPessoais from './DadosPessoais';
+
+function FormularioCadastro({ aoEnviar, validarCPF }) {
+    
+
+    return (
+        <DadosPessoais />
+
+    )
+}
+
+export default FormularioCadastro;
