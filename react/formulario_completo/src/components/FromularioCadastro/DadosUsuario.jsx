1270a9794335804b08924cae17013a4159873970
Ricardo Correa Ribeiro <ricardocribeiro@hotmail.com>
2021-10-23 23:37:21 -0300

    Funções dentro do JSX e reutilização de componentes

--- react/formulario_completo/src/components/FromularioCadastro/DadosUsuario.jsx
+++ react/formulario_completo/src/components/FromularioCadastro/DadosUsuario.jsx
@@ -1,13 +1,19 @@
 import { Button, TextField } from '@material-ui/core';
 import React from 'react';
 
-function DadosUsuario() {
+function DadosUsuario({aoEnviar}) {
     return (
-        <form>
+        <form onSubmit={(event)=>{
+            event.preventDefault();
+            aoEnviar();
+        }}
+        >
+
             <TextField
                 id='email'
                 label='email'
                 type='email'
+                required //Faz o campo ser obrigatorio
                 variant='outlined'
                 margin='normal'
                 fullWidth //Aqui é o comendo que completa a linha inteir
@@ -17,6 +23,7 @@ function DadosUsuario() {
                 id='senha'
                 label='senha'
                 type='password'
+                required //Faz o campo ser obrigatorio
                 variant='outlined'
                 margin='normal'
                 fullWidth
--- react/formulario_completo/src/components/FromularioCadastro/FormularioCadastro.jsx
+++ react/formulario_completo/src/components/FromularioCadastro/FormularioCadastro.jsx
@@ -1,19 +1,27 @@
-import React from 'react';
+
+import React, { useState } from 'react';
 import DadosEntrega from './DadosEntrega';
 import DadosPessoais from './DadosPessoais';
 import DadosUsuario from './DadosUsuario';
 
 function FormularioCadastro({ aoEnviar, validarCPF }) {
+    const [etapaAtual, setEtapaAtual] = useState(0);
 
+    const formularios = [
+        <DadosUsuario aoEnviar={proximo} />,
+        <DadosPessoais aoEnviar={proximo} validarCPF={validarCPF} />,
+        <DadosEntrega aoEnviar={aoEnviar} />
+    ];
 
-    return (
-        <>
-            <DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF} />
-            <DadosUsuario />
-            <DadosEntrega />
-        </>
+    function proximo() {
+        setEtapaAtual(etapaAtual + 1);
+    }
 
-    )
+    // return <Typography>Erro ao selecinaor formulário</Typography>
+
+    return <>{formularios[etapaAtual]}</>;
+    
 }
+
 
 export default FormularioCadastro;
