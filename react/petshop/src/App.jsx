316f4eb19896ee4ca8f49d7cb0bbeb9d45b6c9ae
Ricardo Correa Ribeiro <ricardocribeiro@hotmail.com>
2021-10-25 20:10:22 -0300

    criação do projeto do blog do PetShop

---
+++ react/petshop/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './assets/css/base/base.css'
+import Home from './paginas/Home';
+import Sobre from './paginas/Sobre'
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route path='/sobre'>
+          <Sobre />
+        </Route>
+      </Switch>
+    </Router>
+  )
+}
+
+export default App
