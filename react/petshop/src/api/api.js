b6aba414dca5410ae153cfd25c880d9929852631
Ricardo Correa Ribeiro <ricardocribeiro@hotmail.com>
2021-10-26 12:24:39 -0300

    criando página 404 função useHistory e criado o filtro por categorias

--- react/petshop/src/api/api.js
+++ react/petshop/src/api/api.js
@@ -4,7 +4,7 @@ export const api = axios.create({
     baseURL: 'http://localhost:5000'
 })
 
-export const busca = async(url, setDado) => {
+export const busca = async (url, setDado) => {
     const resposta = await api.get(url)
     setDado(resposta.data)
 }
--- react/petshop/src/components/ListaPost.jsx
+++ react/petshop/src/components/ListaPost.jsx
@@ -7,13 +7,14 @@ const ListaPost = ({ url }) => {
 
     useEffect(() => {
         busca(url, setPosts)
-    }, [])
+    }, [url])
 
     return (
         <section className='posts container'>
             {
                 posts.map((post) => (
-                    <Link className={`cartao-post cartao-post--${post.categoria}`}>
+                    <Link className={`cartao-post cartao-post--${post.categoria}`}
+                        to={`/posts/${post.id}`}>
                         <article Key={post.id}>
                             <h3 className='cartao-post__titulo'>
                                 {post.title}
