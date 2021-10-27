e4c48ecb074fb6d086a26dc2778830e5954e9e04
Ricardo Correa Ribeiro <ricardocribeiro@hotmail.com>
2021-10-27 06:01:58 -0300

    finalizado o projeto, usamos o Themeproviser do StyledComponents

--- react/ByteBank/src/App.js
+++ react/ByteBank/src/App.js
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import { ThemeProvider } from "styled-components";
+import { temaClaro, temaEscuro } from "./Components/UI/temas";
 
 import Cabecalho from "./Components/Cabecalho";
 import Container from "./Components/Container";
-import {GlobalStyle} from './Components/GlobalStyle'
+import { GlobalStyle } from './Components/GlobalStyle';
+import { BtnTema } from "./Components/UI";
+import SwitcherTema from './Components/SwitcherTema';
 
 function App() {
+  const [tema, setTema] = useState(true);
+
+  const toggetTema = () => {
+    setTema((tema) => !tema);
+  };
+
   return (
-    <>
+    //O ThemeProvider faz a escolha do Tema se é escuro ou claro 
+    <ThemeProvider theme={tema ? temaClaro : temaEscuro}>
       <GlobalStyle />
+      <BtnTema onClick={toggetTema}>
+        <SwitcherTema tema={tema} />
+      </BtnTema>
       <Cabecalho />
       <Container />
-    </>
+    </ThemeProvider>
   );
 }
 
